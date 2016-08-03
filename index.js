277c0f4607ac7a6f6ce5aa3fb49e46b438015ef4
Greger Hälltorp <greger.halltorp@gmail.com>
2016-08-03 08:38:43 +0200

    all tests green

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
-require('babel-register');
-require('babel-polyfill');
-const server = require('./server').server;
-server();
+import { Transform } from 'stream';
+import { createHash } from 'crypto';
+
+const splitRegex = /(\d{2}\/\d{2}\/\d{4},\s{1}\d{2}:\d{2}\s{1})/;
+const dateRegex = /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2})/;
+const msgRegex = /^- (.*): ((?:.|\n)*)/;
+
+const parseDatePart = (data) => {
+  if (!data.length) {
+    throw new Error('length cannot be zero in dateparse');
+  }
+  const result = data.match(dateRegex);
+  const year = parseInt(result[3], 10);
+  const month = parseInt(result[2], 10) - 1;
+  const day = parseInt(result[1], 10);
+  const date = new Date(year, month, day);
+  const hour = parseInt(result[4], 10);
+  const minute = parseInt(result[5], 10);
+  date.setHours(hour, minute);
+  return date;
+};
+
+const parseMessagePart = (data) => {
+  if (!data.length) {
+    throw new Error('length cannot be zero in messageparse');
+  } else {
+    const result = data.match(msgRegex);
+    return {
+      name: result[1],
+      body: result[2].trim(),
+    };
+  }
+};
+
+class Parser extends Transform {
+  constructor(options) {
+    const newOptions = {
+      ...options,
+      objectMode: true,
+    };
+    super(newOptions);
+    this.objectMode = true;
+
+    this.message = {};
+  }
+
+  _transform(data, encoding, done) {
+    if (splitRegex.test(data)) {
+      this.message.date = parseDatePart(data);
+    } else if (data.length && data.indexOf(':') > -1) {
+      const parsedMessage = parseMessagePart(data);
+      this.message.name = parsedMessage.name;
+      this.message.body = parsedMessage.body;
+      this.message.words = parsedMessage.body.split(/\s/);
+      this.message.hashId = createHash('sha256')
+        .update(`${this.message.date}${this.message.name}${this.message.body}`)
+        .digest('hex');
+      this.push(this.message);
+      this.message = {};
+    } else {
+      this.message = {};
+    }
+    done();
+  }
+}
+
+export default Parser;
+module.exports = Parser;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,50 +1,110 @@
+/* eslint-disable no-unused-expressions */
 import { expect } from 'chai';
-import { parseDatePart, parseMessagePart } from '../server';
+import { createReadStream } from 'fs';
+import { split, through } from 'event-stream';
+import Parser from '../index';
 
+const setup = () => {
+  const splitRegex = /(\d{2}\/\d{2}\/\d{4},\s{1}\d{2}:\d{2}\s{1})/;
+  const parser = new Parser();
 
-describe('messageParsing', () => {
+  const getMessages = new Promise(
+    (resolve, reject) => { // eslint-disable-line no-unused-vars
+      const tempMsgs = [];
+      createReadStream('test.txt', { flags: 'r' })
+      .pipe(split(splitRegex))
+      .pipe(parser)
+      .pipe(through(
+        data => {
+          tempMsgs.push(data);
+        },
+        () => {
+          resolve(tempMsgs);
+        }
+      ));
+    }
+  );
+
+  return getMessages;
+};
+
+describe('Message Parsing', () => {
+  let messages = [];
+  before(() => setup().then(result => { messages = result; }));
+
+  describe('Basic stuff', () => {
+    it('should make messages an array', () => {
+      expect(messages).to.be.an('array');
+    });
+    it('should contain the correct number of messages', () => {
+      expect(messages.length).to.equal(26);
+    });
+    it('should make each message a non-null object', () => {
+      messages.forEach(message => {
+        expect(message).to.not.be.null;
+        expect(message).to.be.an('object');
+      });
+    });
+    it('should give all messages a date property of Date type', () => {
+      messages.forEach(message => {
+        expect(message).to.have.property('date');
+        expect(message.date).to.be.a('date');
+      });
+    });
+    it('should give all messages a name property of string type', () => {
+      messages.forEach(message => {
+        expect(message).to.have.property('name');
+        expect(message.name).to.be.a('string');
+      });
+    });
+    it('should give all messages a body property of string type', () => {
+      messages.forEach(message => {
+        expect(message).to.have.property('body');
+        expect(message.body).to.be.a('string');
+      });
+    });
+    it('should give all messages a hashId property of string type', () => {
+      messages.forEach(message => {
+        expect(message).to.have.property('hashId');
+        expect(message.hashId).to.be.a('string');
+      });
+    });
+    it('should give all messages a words property of array type', () => {
+      messages.forEach(message => {
+        expect(message).to.have.property('words');
+        expect(message.words).to.be.an('array');
+      });
+    });
+  });
+
   describe('parseDatePart', () => {
-    const dateString = '23/10/2015, 20:57';
-    const dateParseResult = parseDatePart(dateString);
-
-    it('should return a Date', () => {
-      expect(dateParseResult instanceof Date).to.equal(true);
+    it('should give first message the correct year', () => {
+      expect(messages[0].date.getFullYear()).to.equal(2015);
     });
-    it('should return the correct year', () => {
-      expect(dateParseResult.getFullYear()).to.equal(2015);
+    it('should give first message the correct month', () => {
+      expect(messages[0].date.getMonth()).to.equal(9);
     });
-    it('should return the correct month', () => {
-      expect(dateParseResult.getMonth()).to.equal(9);
+    it('should give first message the correct day', () => {
+      expect(messages[0].date.getDate()).to.equal(23);
     });
-    it('should return the correct day', () => {
-      expect(dateParseResult.getDate()).to.equal(23);
+    it('should give first message the correct hour', () => {
+      expect(messages[0].date.getHours()).to.equal(20);
     });
-    it('should return the correct hour', () => {
-      expect(dateParseResult.getHours()).to.equal(20);
-    });
-    it('should return the correct minute', () => {
-      expect(dateParseResult.getMinutes()).to.equal(57);
+    it('should give first message the correct minute', () => {
+      expect(messages[0].date.getMinutes()).to.equal(57);
     });
   });
 
   describe('parseMessagePart', () => {
-    const messageString = '- Markus Holmgren: Hej hej,\nTerminal och tid för utflight?\n(Då kan man gräva själv ang flighten...😉)\nDet vi undrar är väsentligt... Det är skillnaden... hihi.\n'; // eslint-disable-line max-len
-    const messageResult = parseMessagePart(messageString);
-
-    it('should return an object', () => {
-      expect(typeof messageResult).to.equal('object');
+    it('should give last message the correct name', () => {
+      expect(messages[messages.length - 1].name).to.equal('Markus Holmgren');
     });
-    it('should contain name property', () => {
-      expect(Object.keys(messageResult).indexOf('name')).to.be.above(-1);
+    it('should remove trailing "\\n"', () => {
+      const thisMessage = messages[messages.length - 1];
+      expect(thisMessage.body[thisMessage.body.length - 1]).to.not.equal('\n');
     });
-    it('should have correct name', () => {
-      expect(messageResult.name).to.equal('Markus Holmgren');
-    });
-    it('should have a body property', () => {
-      expect(Object.keys(messageResult).indexOf('body')).to.be.above(-1);
-    });
-    it('body should not end in "\n"', () => {
-      expect(messageResult.body[messageResult.body.length - 1]).to.not.equal('\n');
+    it('should make the word list of the correct length', () => {
+      expect(messages[messages.length - 1].words.length).to.equal(27);
     });
   });
 });
